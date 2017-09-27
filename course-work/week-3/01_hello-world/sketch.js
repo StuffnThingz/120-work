5529cc24579c5587faec24db204499fe35074780
StuffnThingz <tobin.long@umconnect.umt.edu>
2017-09-26 21:30:10 -0600

    Adding coursework p5 examples and committing hw-3

---
+++ course-work/week-3/01_hello-world/sketch.js
@@ -0,0 +1,3 @@
+function setup(){
+    text("Hello Tobin!", 0, 10);
+}
---
+++ hw-3/sketch.js
@@ -0,0 +1,14 @@
+// Type in function setup
+function setup() {
+  // create 600 x 400px canvas
+  createCanvas( 600, 400 );
+
+  //add pink background color
+  background( 'pink' );
+}
+
+//Declare draw() function
+function draw() {
+  //Using the text function, use p5 to write, "Hello World!"
+  text( "Hello World!", 300, 200 );
+}
