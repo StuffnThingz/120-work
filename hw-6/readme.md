b2cf799812d059509b79fb6aacdd34b4dce4ed92
StuffnThingz <tobin.long@umconnect.umt.edu>
2017-10-19 15:09:52 -0600

    final commit

--- hw-6/readme.md
+++ hw-6/readme.md
@@ -1,3 +1,20 @@
-Empty p5 sketch.
+Tobin Long, 50
 
-Please replace contents of this file with appropriate readme information after finishing your p5 sketch.
+[Sketch]https://stuffnthingz.github.io/120-work/hw-6/
+
+# HW-5 RESPONSE
+
+Wow, that was a lot to take in. The math is makeing me nervous for some reason.
+I guess I mostly had a hard time trying to come up with original ideas to use with
+all this new info in a short period of time.
+
+I found the random and map functions most interesting this week. Still haven't worked
+out the kinks with the map function... For instance, I wanted to make my circles turn from blue to red with
+mouseX, but just settled for them going from black to red. I tried to utilize them
+contrain function but couldn't get it to do what I wanted. I wanted the spots to stay
+within the boundries, but it was doing all kinds of weird stuff. I was kind of cool, I could
+have just lied and said that's what I wanted it to do, but that's counter productive.
+
+I wasn't able to answer anyone's issues this week, but it looks like a couple people
+had the same concerns about meeting all the math requirements. It looked like someone else
+had a similar problem with getting the map function to work with coloring a shape.
--- hw-6/sketch.js
+++ hw-6/sketch.js
@@ -1,19 +1,22 @@
+//set values for big circles
 var circle = {};
   circle.pos_x = 768;
   circle.pos_y = 367;
   circle.size = 100;
 
+//set values for spots. Not sure if my math worked, but i wanted it to be on -
+//just the the left side of the screen.
 var spot = {};
-spot.posX = 750;
-spot.posY = 350;
+spot.posX = windowHeight / 2;
+spot.posY = abs(-19)**2;
 
-
+//set color values
 var col = {
   r: 255,
-  g: 0,
+  g: 255,
   b: 255
 }
-
+//setup fbackground and framerate
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background('rgb(49, 50, 49)');
@@ -23,20 +26,26 @@ function setup() {
 
 function draw() {
     push();
+    //random function for where circles show up
     circle.pos_x = random(0, width);
     circle.pos_y = random(0, height);
+    //make colors go from dark to red with mouseX
     col = map(mouseX, 0, windowWidth, 0, 255);
     fill(col, 0, 0, random(100, 255));
     noStroke();
+    //make circle size random
     ellipse(circle.pos_x, circle.pos_y, random(10, circle.size));
     pop();
 
     push();
+    //random function for where spots show up
     spot.posX = random(0, width);
     spot.posY = random(0, height);
+    //make colors go from dark to green with mouseX
     col = map(mouseX, 0, windowWidth, 0, 255);
-    stroke(0, 0, col);
-    strokeWeight(10);
-    point(spot.posX, spot.posY);
+    stroke(0, col, 0);
+    strokeWeight(5);
+    //make spots show up on left side of screen I think.
+    point(spot.posX / 2, spot.posY * ceil(.5));
     pop();
 }
