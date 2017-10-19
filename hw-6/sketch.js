98dea59d9a114d81c2ea7a2fbbebf372a297c941
StuffnThingz <tobin.long@umconnect.umt.edu>
2017-10-19 00:59:29 -0600

    commit 2

--- hw-6/sketch.js
+++ hw-6/sketch.js
@@ -3,6 +3,11 @@ var circle = {};
   circle.pos_y = 367;
   circle.size = 100;
 
+var spot = {};
+spot.posX = 750;
+spot.posY = 350;
+
+
 var col = {
   r: 255,
   g: 0,
@@ -17,11 +22,21 @@ function setup() {
 }
 
 function draw() {
+    push();
     circle.pos_x = random(0, width);
     circle.pos_y = random(0, height);
     col = map(mouseX, 0, windowWidth, 0, 255);
     fill(col, 0, 0, random(100, 255));
     noStroke();
     ellipse(circle.pos_x, circle.pos_y, random(10, circle.size));
+    pop();
 
+    push();
+    spot.posX = random(0, width);
+    spot.posY = random(0, height);
+    col = map(mouseX, 0, windowWidth, 0, 255);
+    stroke(0, 0, col);
+    strokeWeight(10);
+    point(spot.posX, spot.posY);
+    pop();
 }
