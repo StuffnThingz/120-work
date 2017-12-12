45a1d10c99dd0a60d572cd8ad72fdb0cc6f59bbe
StuffnThingz <tobin.long@umconnect.umt.edu>
2017-12-12 16:30:16 -0700

    commit for questions 2

--- practice folder/bubbles.js
+++ practice folder/bubbles.js
@@ -1,41 +1,39 @@
-class Ball {
-  constructor() {
-  this.x = random(0, windowWidth);
-  this.y = random(0, windowHeight);
-  this.r = random(10, 75);
+function Ball(x, y, r) {
+  this.x = x;
+  this.y = y;
+  this.r = r;
   this.delta_x = random(2, -2);
   this.delta_y = random(2, -2);
   this.scale_x = random(2, -2);
   this.scale_y = random(2, -2);
-  this.col =('rgb(255, 32, 10)');
 
-  this.intersects = function(other) {
-    let d = dist(this.x, this.y, other.x, other.y);
+this.display = function() {
+  strokeWeight(2);
+  fill('rgb(255, 32, 10)');
+  ellipse(this.x, this.y, this.r * 2, this.r *2);
+}
+
+this.intersects = function(other) {
+  let d = dist(this.x, this.y, other.x, other.y);
     if (d < this.r + other.r) {
       return true;
     } else {
       return false;
     }
   }
-}
 
-
-  move() {
+  this.update = function() {
     this.x += this.delta_x;
     this.y += this.delta_y;
+}
+
+  this.edgeCheck = function() {
     if (this.x + this.r >= width || this.x - this.r <= 0) {
         this.delta_x *= -1;
     }
     if (this.y + this.r >= height || this.y - this.r <= 0) {
         this.delta_y *= -1;
 
-  }
-}
-
-  show() {
-    stroke('rgb(0, 0, 0)');
-    strokeWeight(3);
-    fill(this.col);
-    ellipse(this.x, this.y, this.r * 2);
+    }
   }
 }
--- practice folder/sketch.js
+++ practice folder/sketch.js
@@ -1,18 +1,20 @@
-let ball = [];
+var ball = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-    for (let i = 0; i < 23; i++) {
-      ball[i] = new Ball();
+    for (var i = 0; i < 20; i++) {
+      ball[i] = new Ball(random(width), random(height), random(10, 75));
     }
 }
 
 function draw() {
   background('rgb(51, 0, 255)')
-    for (let i = ball.length - 1; i >= 0; i--) {
-      ball[i].show();
-      ball[i].move();
-     for (let j = ball.length - 1; j >= 0; j--) {
+
+    for (var i = ball.length - 1; i >= 0; i--) {
+      ball[i].display();
+      ball[i].update();
+      ball[i].edgeCheck();
+     for (var j = 0; j < ball.length; j++) {
        if (i != j && ball[i].intersects(ball[j])) {
          ball.splice(i, 1);
          ball.splice(j, 1);
